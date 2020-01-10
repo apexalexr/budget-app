05cba76403c20ccd194ec6b168d9d3cfba546441
Ho Kwan Alex Yeung <hokwanalex.yeung@ad.infosys.com>
2020-01-10 14:02:51 -0600

    Initial Commit

---
+++ budget/classes/Budget.js
@@ -0,0 +1,51 @@
+let SubBudget = require('./subBudget.js')
+
+class Budget {
+    constructor(income, month, year) {
+        this.month = month;
+        this.year = year;
+        this.income = income;
+        this.costs = [];
+        this.subBudgets = [];
+    }
+
+    addCost (descr, amount, isCat) {
+        let temp  = new Cost(descr, amount, isCat)
+        if(isCat) {
+            this.subBudgets.push(temp)
+        } else {
+            this.costs.push(temp)
+        }
+    }
+
+    addSub(sub) {
+        let temp = sub;
+        this.subBudgets.push(temp)
+    }
+
+    calculateRemaining() {
+        let totalCosts = 0;
+        this.costs.forEach(element => {
+            totalCosts += element.amount
+        })
+        let totalSubBudgets = 0;
+        this.subBudgets.forEach(element => {
+            totalSubBudgets += element.allocated
+        })
+        return this.income-totalCosts-totalSubBudgets
+    }
+}
+
+class Cost {
+    constructor(descr, amount, isCat) {
+        this.descr = descr
+        this.amount = amount
+        this.isCat = isCat
+        if(isCat) {
+            let temp = new SubBudget(descr, amount);
+            return temp
+        }
+    }
+}
+
+module.exports = Budget
