034fbf68b55de6b76ea1cea830b006ca06df02be
Ho Kwan Alex Yeung <hokwanalex.yeung@ad.infosys.com>
2020-01-14 10:49:35 -0600

    Initial commit of express and react

--- budget/app.js
+++ budget/app.js
@@ -1,20 +1,9 @@
 const Budget = require('./classes/Budget.js')
 const SubBudget = require('./classes/subBudget.js')
 
-// incomeCosts.updateIncome(100)
-
-// console.log(incomeCosts.iC)
-
-// incomeCosts.addCost('Range', 201.29)
-
-// let newSub = new SubBudget('Food',200)
-// let nextSub = new SubBudget('not',500)
-// newSub.addUse('Seiwa',100)
-// nextSub.addUse('books', 50)
-// console.log(newSub,nextSub)
-
 let newBudget = new Budget(3000);
 newBudget.addCost('rent', 250, false);
 newBudget.addCost('food', 200, true);
 console.log(newBudget)
-console.log(newBudget.calculateRemaining())
+console.log("The remaining budget is ",newBudget.calculateRemaining())
+
