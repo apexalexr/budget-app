2e59a940b82b74f325f808be6f2ca27a5105ccc8
Ho Kwan Alex Yeung <hokwanalex.yeung@ad.infosys.com>
2020-01-16 16:22:40 -0600

    Added mongo connection

--- budget/src/app.js
+++ budget/src/app.js
@@ -1,4 +1,16 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://budget:budget@cluster0-6sw4j.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log(client.isConnected())
+  console.log(err)
+  // perform actions on the collection object
+  client.close();
+});
+
 const Budget = require('../classes/Budget.js')
+const mongodb = require('mongodb')
 const express = require('express')
 const hbs = require('hbs')
 const path = require('path')
@@ -16,26 +28,27 @@ hbs.registerPartials(partialsPath)
 
 try {
   let newBudget = new Budget(3000,1,2020);
-  newBudget.addCost('rent', 250);
-  newBudget.addSub('food', 200);
-  newBudget.addSub('range',100)
-  newBudget.addSubBudgetUse('food', 'Seiwa', 5)
-  newBudget.addSubBudgetUse('food', 'Seid', 5)
-  console.log(newBudget)
-  console.log("The remaining budget is ",newBudget.calculateRemaining())
-  // let {descr, allocated,uses,remaining} = newBudget.subBudgets[0]
-  console.log(newBudget.subBudgets[0].usage)
 } catch (error) {
   console.log(error.message)
 }
 
+let newBudget = new Budget(3000,1,2020)
+newBudget.addCost('rent', 250);
+newBudget.addSub('food', 200);
+newBudget.addSub('range',100)
+newBudget.addSubBudgetUse('food', 'Seiwa', 5)
+newBudget.addSubBudgetUse('food', 'Seid', 5)
+console.log(newBudget)
+console.log("The remaining budget is ",newBudget.calculateRemaining())
+console.log(newBudget.subBudgets[0].usage)
 
+console.log(newBudget)
 
 
 app.get('/', function (req, res) {
-    res.render('budget', {
-      subBudgets : newBudget.subBudgets
-    })
+  res.render('budget', {
+    subBudgets : newBudget.subBudgets
   })
+})
    
 app.listen(3000)
