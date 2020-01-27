28b03c2ff2fbe53a4a864d625211ef41ce98f869
Ho Kwan Alex Yeung <hokwanalex.yeung@ad.infosys.com>
2020-01-27 17:55:25 -0600

    Finished basic REST CRUD for Cost starting SubBudget

--- budget/schema/schema.js
+++ budget/schema/schema.js
@@ -1,35 +1,130 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
+var CostSchema = new Schema({
+  descr: {type: String},
+  amount : Number
+})
+var useSchema = new Schema({
+  descr : String,
+  amount : Number
+})
+var SubBudgetSchema = new Schema({
+  descr : String,
+  allocated : Number,
+  usage : [useSchema],
+  remaining: Number
+})
+
 var budgetSchema = new Schema({
-    month : {type: Number, min: 1, max: 12},
-    year : {type: Number},
-    income : Number,
-    costs : [{
-      descr : String,
-      amount : Number
-    }],
-    sub_budgets : [{
-      descr : String,
-      allocated : Number,
-      usage : [{
-        descr : String,
-        amount : Number
-      }],
-      remaining : Number
-    }]
-  })
-  // Budget Methods
-  budgetSchema.methods.getIncome = function() {
-    console.log(this.income)
+  month : {type: Number, min: 1, max: 12},
+  year : {type: Number},
+  income : Number,
+  costs : [CostSchema],
+  sub_budgets : [SubBudgetSchema],
+  remaining : Number
+})
+
+//Custom Array Searching and Unique Checking
+budgetSchema.methods.uniqueCheck = function(array , descr) {
+  if(array === "cost") {
+    return (this.findIndex(array,descr) == -1)
+  } else if(array === "sub") {
+    return (this.findIndex(array,descr) == -1)
   }
-  //Cost Methods
-  budgetSchema.methods.addCost = (descr,amount) => {
-    this.costs.push({descr,amount})
-    console.log("Successfully added cost " + descr + " amount " + amount + "!")
+}
+
+budgetSchema.methods.findIndex = function(array , descr) {
+  if(array === "cost") {
+    return this.costs.findIndex((element)=> {
+      return (element.descr === descr)
+    })
+  } else if(array === "sub") {
+    //change to subs
+    return this.sub_budgets.findIndex((element)=> {
+      return (element.descr === descr)
+    })
   }
+}
 
+//Cost Methods CRUD
+budgetSchema.methods.addCost = function(descr,amount) {
+  if(!this.uniqueCheck("cost", descr)) {
+    return null;
+  }
+  this.costs.push({descr,amount})
+  this.save()
+  return true;
+}
 
-  var Budget = mongoose.model('Budget', budgetSchema)
+budgetSchema.methods.getCost = function(descr) {
+  let index = this.findIndex("cost",descr)
+  if(index == -1) {
+    console.log("No cost named " + descr + " found!")
+    return null;
+  }
+  return this.costs[index]
+}
 
-  module.exports = Budget
+budgetSchema.methods.deleteCost = function(descr) {
+  let index = this.findIndex("cost",descr)
+  if(index == -1) {
+    return null
+  }
+  this.costs.splice(index,1)
+  this.save()
+  return true
+}
+
+budgetSchema.methods.updateCost = function(descr,amount) {
+  let index = this.findIndex("cost",descr)
+  if(index == -1) {
+    return null
+  }
+  this.costs[index] = {descr,amount}
+  this.save()
+  return true
+}
+
+//SubBudget Methods
+budgetSchema.methods.addCost = function(descr,amount) {
+  if(!this.uniqueCheck("cost", descr)) {
+    return null;
+  }
+  this.costs.push({descr,amount})
+  this.save()
+  return true;
+}
+
+budgetSchema.methods.getCost = function(descr) {
+  let index = this.findIndex("cost",descr)
+  if(index == -1) {
+    console.log("No cost named " + descr + " found!")
+    return null;
+  }
+  return this.costs[index]
+}
+
+budgetSchema.methods.deleteCost = function(descr) {
+  let index = this.findIndex("cost",descr)
+  if(index == -1) {
+    return null
+  }
+  this.costs.splice(index,1)
+  this.save()
+  return true
+}
+
+budgetSchema.methods.updateCost = function(descr,amount) {
+  let index = this.findIndex("cost",descr)
+  if(index == -1) {
+    return null
+  }
+  this.costs[index] = {descr,amount}
+  this.save()
+  return true
+}
+
+var Budget = mongoose.model('Budget', budgetSchema)
+
+module.exports = Budget
--- budget/src/app.js
+++ budget/src/app.js
@@ -1,5 +1,4 @@
 const Budget = require('../schema/schema.js')
-// const Budget = require('../classes/Budget.js')
 const express = require('express')
 const hbs = require('hbs')
 const path = require('path')
@@ -12,11 +11,7 @@ const viewsPath = path.join(__dirname,'../templates/views')
 const partialsPath = path.join(__dirname,'../templates/partials')
 
 //Connect to MongoClient Budget cluster
-
-console.log(Budget)
 const uri = "mongodb+srv://budget:budget@cluster0-6sw4j.mongodb.net/test?retryWrites=true&w=majority";
-
-
 
 mongoose.connect(uri, {
   useNewUrlParser: true,
@@ -27,31 +22,71 @@ var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error: '));
 db.once('open', function () {
-  let tempBudget = new Budget({
-    month : 1,
-    income : 3000})
-  tempBudget.getIncome()
-  tempBudget.save()
-  // Budget.find((err,budget) => {
-  //   if(err) return console.error(err);
-  //   console.log(budget)
-  // })
+  console.log("Connection Open")
 })
-
 
 app.set('view engine', 'hbs')
 app.set('views',viewsPath)
 hbs.registerPartials(partialsPath)
 
-// app.get('/', function (req, res) {
-//   res.render('budget', {
-//     subBudgets : newBudget.subBudgets
-//   })
-// })
+app.use(express.json())
 
-app.post('/addBudget', function(req,res) {
-  console.log(req.body)
+app.post('/addBudget', (req,res) => {
+  let tempBudget = new Budget(req.body)
+  tempBudget.save()
   res.status(200).send('Success!')
 })
-   
+
+//Cost CRUD methods
+app.put('/addCost', (req,res) => {
+  Budget.findOne({_id : req.body._id}, (err,budget) => {
+    if(err) console.error(err)
+    if(budget.addCost(req.body.descr,req.body.amount)) {
+      res.status(200).send("Successfully added cost " + req.body.descr + " amount " + req.body.amount + "!")
+    } else {
+      res.status(404).send("This description has already been used.")
+    }
+  })
+})
+
+app.get('/getCost', (req,res) => {
+  Budget.findOne({_id : req.body._id}, (err,budget) => {
+    if(err) console.error(err)
+    let tempCost = budget.getCost(req.body.descr)
+    if(tempCost) {
+      console.log("Cost Found :")
+      console.log("------------")
+      console.log("Descr : " + tempCost.descr)
+      console.log("Amount: " + tempCost.amount)
+      res.status(200).send({descr:tempCost.descr,amount : tempCost.amount})
+    } else {
+      res.status(404).send("No cost named " + req.body.descr + " found!")
+    }
+  })
+})
+
+app.delete('/deleteCost', (req,res) => {
+  Budget.findOne({_id : req.body._id}, (err,budget) => {
+    if(err) console.error(err)
+    if(budget.deleteCost(req.body.descr)) {
+      res.status(200).send(req.body.descr + " has been deleted.")
+    } else {
+      res.status(404).send(req.body.descr + " does not exist!")
+    }
+  })
+})
+
+app.put('/updateCost',(req,res) => {
+  Budget.findOne({_id : req.body._id}, (err,budget) => {
+    if(err) console.error(err)
+    if(budget.updateCost(req.body.descr,req.body.amount)) {
+      res.status(200).send("Cost " + req.body.descr + " has been updated.")
+    } else {
+      res.status(404).send("Cost not found")
+    } 
+  })
+})
+
+
+//SubBudget CRUD Method Endpoints
 app.listen(3000)
