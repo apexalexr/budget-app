3ccf7e57526a4a5d9ce4676cb6732b3cde9df9c5
Ho Kwan Alex Yeung <hokwanalex.yeung@ad.infosys.com>
2020-03-02 12:02:48 -0600

    Added SubBudget

--- view/src/App.js
+++ view/src/App.js
@@ -1,30 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Budget from './components/budget.js'
 import data from './test.json'
+import SubBudget from './componente/subbudget.js'
 
 function App() {
+  console.log("starting app")
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //       <Budget />
-    //     </a>
-    //   </header>
-    // </div>
-    <Budget data={data}/>
-  );
+      <Budget data={data}/>
+      <SubBudget />
+    );
 }
 
 export default App;
